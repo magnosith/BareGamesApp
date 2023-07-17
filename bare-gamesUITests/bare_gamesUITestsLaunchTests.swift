ee3708827c8a462562955ca1b11015b34b882d7b
magnosith <magnosith@icloud.com>
2023-07-17 12:22:06 -0700

    Initial Commit

---
+++ bare-gamesUITests/bare_gamesUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  bare_gamesUITestsLaunchTests.swift
+//  bare-gamesUITests
+//
+//  Created by Student on 17/07/23.
+//
+
+import XCTest
+
+class bare_gamesUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
